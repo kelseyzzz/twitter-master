50e603740cab14ee9228dbb77478d0be0920435b
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-05-01 14:26:33 -0400

    finished

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$twitteruser = "twitterusername";
+
+$consumerkey = "fzsfNnJ2iX6PHiw0LToJVQW2m";
+$consumersecret = "nRtNIATpZ82yJs7JZOiQgFcqufbnvxmdgr0Ec1QOuLcUcZZ8IA";
+$accesstoken = "2238592014-Jr3yMjJq9p0wCXz6v2ste22EZcuFtUhmnEIR5r5";
+$accesstokensecret = "Ua2NQpPbB2PnW7j04I09X7DQfnm0gPQ1gmKMF7exdr1ZA";
--- js/week5.js
+++ js/week5.js
@@ -4,11 +4,73 @@ var TwitterApi = (function(options) {
 	var options = options || {};
 
 	function setupListeners() {
+
 		console.log('setupListeners()');
+
+		$('.timeline').on('click', function(e) {
+			e.preventDefault();
+			killTheTweets();
+			$.ajax('twitter-proxy.php?op=user_timeline&screen_name=' + $('.handle').val())
+			.done(processTweetResults);
+
+		});
+
+		$('.quicky').click('submit', function(e) {
+			e.preventDefault();
+			killTheTweets();
+			$.ajax('twitter-proxy.php?op=search_tweets&q=' + $('.query-search').val())
+	
+			.done(processSearchTweets);
+
+		});
+
+		$('.custom').click('submit', function(e) {
+			e.preventDefault();
+			killTheTweets();
+			$.ajax('twitter-proxy.php?op=search_tweets&q=' + $('.query-2').val() + '&count=' + $('.limit').val() + '&result_type=' + $('.result_type').val())
+	
+			.done(processSearchTweets);
+		});
+	}
+
+	function processTweetResults(result) {
+		var results = JSON.parse(result);
+		console.log(results);
+
+		for (var i = 0; i < results.length; i++) {
+			results[i]
+			var timelineResult = document.createElement('p');
+			$(timelineResult).html(results[i].text)
+			$('.tweets').append(timelineResult);
+		}
+
+	}
+
+	function processSearchTweets(result) {
+		var results = JSON.parse(result).statuses;
+		console.log(results);
+
+		for (var i = 0; i < results.length; i++) {
+			results[i]
+			var timelineResult = document.createElement('p');
+			$(timelineResult).html(results[i].text)
+			$('.tweets').append(timelineResult);
+		}
+
+	}
+
+	function killTheTweets() {
+		for (var i = 0; i < $('p').length; i++) {
+			$('p').remove();
+		}
+
 	}
 
 	var init = function() {
 		console.log('init()');
+		setupListeners();
+		// $.ajax('twitter-proxy.php?op=search_tweets&q=france')
+		// .done(processTweetResults)
 	};
 	shared.init = init;
 
