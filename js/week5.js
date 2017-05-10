e1b88fd77ac8a20212c96f50c0449aafb7c28ab8
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-05-10 16:58:27 -0400

    homework 2

--- js/week5.js
+++ js/week5.js
@@ -2,7 +2,7 @@
 var TwitterApi = (function(options) {
 	var shared = {};
 	var options = options || {};
-
+		var value;
 	function setupListeners() {
 
 		console.log('setupListeners()');
@@ -10,7 +10,9 @@ var TwitterApi = (function(options) {
 		$('.timeline').on('click', function(e) {
 			e.preventDefault();
 			killTheTweets();
-			$.ajax('twitter-proxy.php?op=user_timeline&screen_name=' + $('.handle').val())
+			value = $('.handle').val(); 
+			$.ajax('twitter-proxy.php?op=user_timeline&screen_name=' + value )
+			
 			.done(processTweetResults);
 
 		});
@@ -18,7 +20,8 @@ var TwitterApi = (function(options) {
 		$('.quicky').click('submit', function(e) {
 			e.preventDefault();
 			killTheTweets();
-			$.ajax('twitter-proxy.php?op=search_tweets&q=' + $('.query-search').val())
+			value = $('.query-search').val();
+			$.ajax('twitter-proxy.php?op=search_tweets&q=' + value)
 	
 			.done(processSearchTweets);
 
@@ -27,37 +30,42 @@ var TwitterApi = (function(options) {
 		$('.custom').click('submit', function(e) {
 			e.preventDefault();
 			killTheTweets();
-			$.ajax('twitter-proxy.php?op=search_tweets&q=' + $('.query-2').val() + '&count=' + $('.limit').val() + '&result_type=' + $('.result_type').val())
+			value = $('.query-2').val();
+			$.ajax('twitter-proxy.php?op=search_tweets&q=' + value + '&count=' + $('.limit').val() + '&result_type=' + $('.result_type').val())
 	
 			.done(processSearchTweets);
 		});
 	}
 
-	function processTweetResults(result) {
-		var results = JSON.parse(result);
-		console.log(results);
-
+	function createTweetOnPage (results) {
+		console.log('fired');
 		for (var i = 0; i < results.length; i++) {
-			results[i]
 			var timelineResult = document.createElement('p');
-			$(timelineResult).html(results[i].text)
+			var a = RegexModule.url(results[i].text);
+			var b = RegexModule.at(a);
+			var c = RegexModule.hash(b);	
+			var d = RegexModule.word(c, value);
+			$(timelineResult).html(d);
 			$('.tweets').append(timelineResult);
 		}
 
 	}
+
+	function processTweetResults(result) {
+		var results = JSON.parse(result);
+		console.log(results);
+		createTweetOnPage(results);
+	}
+
 
 	function processSearchTweets(result) {
 		var results = JSON.parse(result).statuses;
 		console.log(results);
-
-		for (var i = 0; i < results.length; i++) {
-			results[i]
-			var timelineResult = document.createElement('p');
-			$(timelineResult).html(results[i].text)
-			$('.tweets').append(timelineResult);
-		}
+		createTweetOnPage(results);
 
 	}
+
+
 
 	function killTheTweets() {
 		for (var i = 0; i < $('p').length; i++) {
@@ -69,12 +77,54 @@ var TwitterApi = (function(options) {
 	var init = function() {
 		console.log('init()');
 		setupListeners();
-		// $.ajax('twitter-proxy.php?op=search_tweets&q=france')
-		// .done(processTweetResults)
+		
 	};
 	shared.init = init;
 
 	return shared;
 }());
+
+var RegexModule = (function(){
+	var shared = {};
+
+	var getURL = function(data) {
+		var urlRegex = /((http|ftp|https)?:\/\/\S+)/g;
+
+		return data.replace(urlRegex, "<a class='url' href='$1'> $1 </a>")
+	}
+	var getAt = function(data) {
+		var atRegex = /(^|\W)(@[a-z\d][\w-]*)/ig;
+
+		return data.replace(atRegex, "<a class='at' href='$2'> $2 </a>")
+	}
+	var getHash = function(data) {
+		var hashRegex = /(^|\W)(#[a-z\d][\w-]*)/ig;
+
+		return data.replace(hashRegex, "<a class'hash' href='$3'> $2 </a>")
+	}
+
+	function highlightWord(data, keyword){
+		 var searchWord = new RegExp(keyword, "gi");
+		 console.log(searchWord);
+
+		 
+		 return data.replace(searchWord, "<span class='search-word'> $1 </span>")
+
+
+	}
+
+
+	shared = {
+		url: getURL,
+		at: getAt,
+		hash: getHash,
+		word: highlightWord
+
+		
+	}
+
+	return shared
+
+})();
 
 TwitterApi.init();
